40eba068dc637ca9727abcdcec31196fa352740a
Gopi Korupolu <go1c4qs@L-SFF2H7GTF1-M.local>
2020-04-28 09:23:14 -0700

    Json to Avro using Jackson API

---
+++ json-to-avro-jackson/src/test/java/com/example/jsontoavro/JsontoavroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsontoavro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsontoavroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
